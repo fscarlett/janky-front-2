fef1fede952f71637014ce422af6a311d261fd44
Fox Scarlett <foxontheone@gmail.com>
2026-01-20 01:42:09 -0800

    places route parameters logic and styles

--- src/pages/Places.tsx
+++ src/pages/Places.tsx
@@ -1,16 +1,25 @@
-import { useParams } from 'react-router'
+import { useParams, Link } from 'react-router'
 
 import '../styles/globals.css'
 import styles from '../styles/Places.module.css'
 
+import { destinations } from '../utils/places'
+
 function Places() {
   const { place } = useParams<{ place: string }>()
+
+  const placeName = destinations[place || '']?.name || 'Unknown Place'
+  const placeDescription =
+    destinations[place || '']?.description || 'watev dude'
 
   return (
     <>
       <div className={styles.main}>
-        <h1> {place} </h1>
-        <p>This is the Places page content.</p>
+        <h1> {placeName} </h1>
+        <p>{placeDescription}</p>
+        <Link to='/play' className={styles.back_link}>
+          Gotta bounce!
+        </Link>
       </div>
     </>
   )
