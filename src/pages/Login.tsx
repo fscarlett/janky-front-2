0408ad0e66436215b1eff163cfba369e071db917
Fox Scarlett <foxontheone@gmail.com>
2026-01-21 21:48:40 -0800

    brighten colors and add login status conditional links for home

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router'
+import { SignIn } from '@clerk/clerk-react'
 import styles from '../styles/App.module.css'
 
 function LoginPage() {
   return (
     <div className={styles.main}>
       <h1>Login / Register</h1>
-      <p>clerk stuff here</p>
+      <div className={styles.clerk_wrapper}>
+        <SignIn />
+      </div>
     </div>
   )
 }
