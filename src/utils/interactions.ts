855b0c9a1debf4fd60e55083d6b0fb626e0dc246
Fox Scarlett <foxontheone@gmail.com>
2026-02-11 18:24:12 -0800

    start fill in interactions

--- src/utils/interactions.ts
+++ src/utils/interactions.ts
@@ -4,12 +4,24 @@ export const interactions: {
   results: string[]
 }[] = [
   {
-    interaction: 'describe interaction a',
+    interaction: 'you see the drummer from your last band',
     options: ['option 1', 'option 2', 'option 3'],
     results: ['result 1', 'result 2', 'result 3'],
   },
   {
-    interaction: 'describe interaction b',
+    interaction: 'you see someone you slept with a year ago',
+    options: ['option 1', 'option 2', 'option 3'],
+    results: ['result 1', 'result 2', 'result 3'],
+  },
+  {
+    interaction:
+      "a total stranger starts talking to you like you're a rock star",
+    options: ['option 1', 'option 2', 'option 3'],
+    results: ['result 1', 'result 2', 'result 3'],
+  },
+  {
+    interaction:
+      'you see the person who gave a terrible review of your last record',
     options: ['option 1', 'option 2', 'option 3'],
     results: ['result 1', 'result 2', 'result 3'],
   },
@@ -27,17 +39,17 @@ export const interactions: {
     results: ['result 1', 'result 2', 'result 3'],
   },
   {
-    interaction: 'describe interaction d',
+    interaction: "you see the A & R person who dropped your best friend's band",
     options: ['option 1', 'option 2', 'option 3'],
     results: ['result 1', 'result 2', 'result 3'],
   },
   {
-    interaction: 'describe interaction e',
+    interaction: 'the person you were hoping to see is not there',
     options: ['option 1', 'option 2', 'option 3'],
     results: ['result 1', 'result 2', 'result 3'],
   },
   {
-    interaction: 'describe interaction f',
+    interaction: 'the person you were hoping to see is there',
     options: ['option 1', 'option 2', 'option 3'],
     results: ['result 1', 'result 2', 'result 3'],
   },
