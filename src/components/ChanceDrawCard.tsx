ffcbf6fa3fe0de789a293499fd92ea819697ff3a
Fox Scarlett <foxontheone@gmail.com>
2026-06-03 17:55:25 -0700

    implement randomise experience card from experiences array

--- src/components/ChanceDrawCard.tsx
+++ src/components/ChanceDrawCard.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router'
 import chanceStyles from '../styles/ChanceCard.module.css'
+import { experiences } from '../utils/experiences'
 
 function ChanceDrawCard() {
+  const experiencesAvailable = experiences.length
+  console.log('experiences available:', experiencesAvailable)
+  const cardNumber: number = Math.floor(Math.random() * experiencesAvailable)
   return (
     <div className={chanceStyles.chance_card_wrapper}>
       <Link
-        to='/experiences/0'
+        to={`/experiences/${cardNumber}`}
         title='have a random experience or are you a pantywaist'
         className={chanceStyles.chance_link}
       >
